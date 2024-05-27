e70bf9b7dd08cb76b79622b1ffbf68e113cf5a5c
nebucanetshar <alhannb@hotmail.com>
2024-05-27 08:50:12 +0200

    modification 25/05

--- grpc/Program.cs
+++ grpc/Program.cs
@@ -1,14 +1,19 @@
+using grpc;
 using grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddGrpc();
 
 var app = builder.Build();
+//builder.Services.AddDbContext<AppDbContext>(options => builder.Configuration.GetConnectionString("npgsql"));
 
-// Configure the HTTP request pipeline.
+
+
+
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<AcegServices>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
