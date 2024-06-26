651229bda3c3be2bd681403b97e0b0b34a0c5715
nebucanetshar <alhannb@hotmail.com>
2024-06-26 14:29:55 +0200

    commit for checkout

--- assembly/Fluxor/Effet.cs
+++ assembly/Fluxor/Effet.cs
@@ -1,23 +1,23 @@
-﻿using assembly.Wrapper.ModelWrapper;
-using assembly.fluxor.Action;
-using Fluxor;
+﻿// using assembly.Wrapper.ModelWrapper;
+// using assembly.fluxor.Action;
+// using Fluxor;
 
-namespace assembly.fluxor;
+// namespace assembly.fluxor;
 
-public class CounterEffet
-{
-    private IgRpcCounterServiceClient gRpcCounterServiceClient{get;set;}
+// public class CounterEffet
+// {
+//     private IgRpcCounterServiceClient gRpcCounterServiceClient{get;set;}
 
-    public CounterEffet( IgRpcCounterServiceClient server)
-    {
-        gRpcCounterServiceClient=server;
-    }
-    [EffectMethod]
-    public async Task ExecuteEffet(ActionInput action, IDispatcher dispatcher)
-    {
-         var responseWrapper = await gRpcCounterServiceClient.CreateTodo(action.Counter);
+//     public CounterEffet( IgRpcCounterServiceClient server)
+//     {
+//         gRpcCounterServiceClient=server;
+//     }
+//     [EffectMethod]
+//     public async Task ExecuteEffet(ActionInput action, IDispatcher dispatcher)
+//     {
+//          var responseWrapper = await gRpcCounterServiceClient.CreateTodo(action.Counter);
         
-         dispatcher.Dispatch(new ActionOutput(responseWrapper.Content));
-    }
+//          dispatcher.Dispatch(new ActionOutput(responseWrapper.Content));
+//     }
 
-}
+// }
